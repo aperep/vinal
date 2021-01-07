be753f82821677cec2d0c489908f6b45fa24e137
Alexander Perepechko <a@perep.ru>
2021-01-07 23:00:01 +0300

    up

--- vinal/.ipynb_checkpoints/test-profiling-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import line_profiler
-from vinal import *
-
-V = VinAl(diag(-1,1,1,1,1,1,1,1,1,1)) # unimodular lattice of signature (9,1)
-V3 = VinAl(diag(-3,5,1))
-profile = line_profiler.LineProfiler()
-V.fundamental_cone()
-
-
